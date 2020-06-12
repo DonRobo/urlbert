984b61c8de71881df7cd6541f499d795cdb6ec6a
Robert Tieber <robert.tieber@gmail.com>
2020-06-12 22:12:03 +0200

    Retry redirect over and over

--- jsRedirect/src/main/kotlin/at/robbert/frontend/redirectMain.kt
+++ jsRedirect/src/main/kotlin/at/robbert/frontend/redirectMain.kt
@@ -7,5 +7,12 @@ external val redirectTo: String
 
 suspend fun main() {
     delay(200)
+    while (true) {
+        doRedirect()
+        delay(500)
+    }
+}
+
+fun doRedirect() {
     window.location.href = redirectTo
 }
